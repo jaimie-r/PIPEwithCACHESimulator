ebdb093899181edd588e3f8ca1d2b6d3d7c18a60
Jaimie Ren <jaimier@cs.utexas.edu>
2023-11-06 14:17:57 -0600

    less cases passed

--- src/pipe/forward.c
+++ src/pipe/forward.c
@@ -11,6 +11,11 @@
  * Implement forwarding register values from
  * execute, memory, and writeback back to decode.
  */
+// forward_reg(src1, src2, out->dst, M_in->dst, W_in->dst,
+                //  M_in->val_ex, M_out->val_ex, M_in->val_b, W_in->val_ex,
+                //  W_in->val_mem, M_in->W_sigs.wval_sel, W_in->W_sigs.wval_sel, X_in->W_sigs.w_enable,
+                //  M_in->W_sigs.w_enable, W_in->W_sigs.w_enable,
+                // &(out->val_a), &(out->val_b));
 comb_logic_t forward_reg(uint8_t D_src1, uint8_t D_src2, uint8_t X_dst, uint8_t M_dst, uint8_t W_dst,
                  uint64_t X_val_ex, uint64_t M_val_ex, uint64_t M_val_mem, uint64_t W_val_ex,
                  uint64_t W_val_mem, bool M_wval_sel, bool W_wval_sel, bool X_w_enable,
@@ -22,11 +27,16 @@ comb_logic_t forward_reg(uint8_t D_src1, uint8_t D_src2, uint8_t X_dst, uint8_t 
     //wtf are M_val_mem / W_val_mem supposed to be used for // M_wval_sel / W_wval_sel
     
     //pointer syntax?,,,,
-    if(X_w_enable){
-        *val_a = X_dst == D_src1 ? X_val_ex : *val_a;
-        *val_b = X_dst == D_src2 ? X_val_ex : *val_b;
-    }
-     if (M_w_enable){
+    if (W_w_enable){
+        if(W_wval_sel){
+            *val_a = W_dst == D_src1 ? W_val_mem : *val_a;
+            *val_b = W_dst == D_src2 ? W_val_mem : *val_b;
+        } else {
+            *val_a = W_dst == D_src1 ? W_val_ex : *val_a;
+            *val_b = W_dst == D_src2 ? W_val_ex : *val_b;
+        }
+    } 
+    if (M_w_enable){
         if(M_wval_sel){
             *val_a = M_dst == D_src1 ? M_val_mem : *val_a;
             *val_b = M_dst == D_src2 ? M_val_mem : *val_b;
@@ -35,14 +45,9 @@ comb_logic_t forward_reg(uint8_t D_src1, uint8_t D_src2, uint8_t X_dst, uint8_t 
             *val_b = M_dst == D_src2 ? M_val_ex : *val_b;
         }
     }
-     if (W_w_enable){
-        if(W_wval_sel){
-            *val_a = W_dst == D_src1 ? W_val_mem : *val_a;
-            *val_b = W_dst == D_src2 ? W_val_mem : *val_b;
-        } else {
-            *val_a = W_dst == D_src1 ? W_val_ex : *val_a;
-            *val_b = W_dst == D_src2 ? W_val_ex : *val_b;
-        }
+    if(X_w_enable){
+        *val_a = X_dst == D_src1 ? X_val_ex : *val_a;
+        *val_b = X_dst == D_src2 ? X_val_ex : *val_b;
     }
 
     return;
--- src/pipe/instr_Decode.c
+++ src/pipe/instr_Decode.c
@@ -270,6 +270,11 @@ comb_logic_t decode_instr(d_instr_impl_t *in, x_instr_impl_t *out) {
 
 
     //call forward reg 
+    //uint8_t D_src1, uint8_t D_src2, uint8_t X_dst, uint8_t M_dst, uint8_t W_dst, 
+                //uint64_t X_val_ex, uint64_t M_val_ex, uint64_t M_val_mem, uint64_t W_val_ex,
+                 //uint64_t W_val_mem, bool M_wval_sel, bool W_wval_sel, bool X_w_enable,
+                 //bool M_w_enable, bool W_w_enable,
+                 //uint64_t *val_a, uint64_t *val_b
     forward_reg(src1, src2, out->dst, M_in->dst, W_in->dst,
                  X_in->val_b, M_in->val_ex, M_in->val_b, W_in->val_ex,
                  W_in->val_mem, M_in->W_sigs.wval_sel, W_in->W_sigs.wval_sel, X_in->W_sigs.w_enable,

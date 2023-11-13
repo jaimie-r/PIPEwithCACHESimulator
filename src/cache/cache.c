e3a221b64e54c42b616a2e9e79d292b9bd9c11ed
Angela Walters <awalters@cs.utexas.edu>
2023-11-13 12:36:06 -0600

    37/40 WE CAN SEE OUR FAMILIES (cats) AGAIN

--- src/cache/cache.c
+++ src/cache/cache.c
@@ -28,7 +28,7 @@
 //test cases w/ respective A B C'
 //gdb --args
 //bin/csim -t testcases/cache/yi2.trace -A 1 -B 2 -C 4 
-//bin/csim -t testcases/cache/yi.trace -A 2 -B 16 -C 512
+//TODO: bin/csim -t testcases/cache/yi.trace -A 2 -B 16 -C 512
 //bin/csim -t testcases/cache/yi.trace -A 8 -B 8 -C 64
 //bin/csim -t testcases/cache/dave.trace -A 1 -B 16 -C 64
 //bin/csim -t testcases/cache/trans.trace -A 1 -B 8 -C 32
@@ -63,12 +63,6 @@ static size_t _log(size_t x) {
     result++;
   }
   return result;
-}
-
-//helper method for getting bitfields (unsigned!!)
-unsigned
-bitfield(int32_t src, unsigned frompos, unsigned width) {
-    return (((unsigned)src >> frompos) & ((1 << width) - 1));
 }
 
 /*
@@ -180,12 +174,14 @@ cache_line_t *select_line(cache_t *cache, uword_t addr) {
     uword_t lowest = next_lru;
     cache_set_t *cset = &cache->sets[I];
     cache_line_t *selection;
+
     for(int i = 0; i < cache->A; i++) {
         if(cset->lines[i].lru <= lowest){
             lowest = cset->lines[i].lru;
             selection = &cset->lines[i]; // check pointers maybe?
         }
     }
+
     return selection;
 }
 
@@ -198,7 +194,7 @@ bool check_hit(cache_t *cache, uword_t addr, operation_t operation) {
     //call get_line to determine if the line exists or not 
     cache_line_t *line = get_line(cache, addr);
     if(line) {
-        line->dirty = (operation == WRITE);
+        line->dirty |= operation == WRITE;
         hit_count++;
         next_lru++;        
         line->lru = next_lru;
@@ -218,9 +214,9 @@ evicted_line_t *handle_miss(cache_t *cache, uword_t addr, operation_t operation,
     evicted_line->data = (byte_t *) calloc(cache->B, sizeof(byte_t));
     /* your implementation */
     size_t b = _log(cache->B);
-    __uint32_t S = cache->C / (cache->A * cache->B);
-    __uint32_t t = addr >> (_log(S) + b);
-    __uint32_t I = (addr >> b) & ((1 << _log(S)) - 1);
+    size_t s = _log(cache->C / (cache->A * cache->B));
+    __uint32_t t = addr >> (s + b);
+    __uint32_t I = (addr >> b) & ((1 << s) - 1);
     bool empty = false;
     //search for empty line 
     cache_set_t *cset = &cache->sets[I];
@@ -238,11 +234,11 @@ evicted_line_t *handle_miss(cache_t *cache, uword_t addr, operation_t operation,
         //fill in evicted_line info 
         evicted_line->valid = line->valid;
         evicted_line->dirty = line->dirty;
-        //need to reconstruct the address 
-        evicted_line->addr = line->tag <<(_log(S) + b);
+        //reconstruct the address 
+        evicted_line->addr = line->tag <<  (s + b);
         evicted_line->addr = evicted_line->addr | (I << b);
-        memcpy(evicted_line->data, line->data, (1<<b));
-        if(evicted_line->dirty == 1){
+        memcpy(evicted_line->data, line->data, (1 << b));
+        if(evicted_line->dirty == true){
             dirty_eviction_count++;
         } else {
             clean_eviction_count++;
@@ -252,10 +248,9 @@ evicted_line_t *handle_miss(cache_t *cache, uword_t addr, operation_t operation,
     //update valid + tag + dirty + lru
     line->valid = true;
     line->dirty = (operation == WRITE);
-    if(operation == WRITE){
-        if(incoming_data){
-            memcpy(line->data, incoming_data, (1<<cache->B));
-        }
+    if(incoming_data && operation == WRITE){
+        line->data = (byte_t *) calloc(cache->B, sizeof(byte_t));
+        memcpy(line->data, incoming_data, (1<<b));
     }
     line->tag = t;
     line->lru = next_lru;
@@ -289,6 +284,7 @@ void set_word_cache(cache_t *cache, uword_t addr, word_t val) {
     for(int i=0; i<8; i++){
         // uword_t cur = addr + i;
         // cache_line_t *line = get_line(cache, cur);
+        //dont need to use offset (copying over all 8 bytes)
         // uword_t index = ((1 << cache->B) -1 ) & cur;
         // line->data[index] = valPtr[i];
     }
